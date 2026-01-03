ec0bf1991abaf02f807e90f7505675b8deb7dc60
Muhammed Suhail <admin@Admins-MacBook-Air-3.local>
2026-01-03 12:41:50 +0530

    Restore mobile build configuration and fixes

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const config: NextConfig = {
+  output: "export", // Required for Capacitor
+  images: {
+    unoptimized: true, // Required for Static Export
+  },
 };
 
-export default nextConfig;
+const makePwa = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+export default makePwa(config);
+
